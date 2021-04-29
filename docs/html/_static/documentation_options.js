9045e8ae7e47d6c7b98aafd18231d468d3095ae3
AWS auto commit <math.dube@gmail.com>
2021-04-29 01:00:47 +0000

    Auto-built and commit new docs version.

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '06 April, 2021	',
+    VERSION: '29 April, 2021	',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
